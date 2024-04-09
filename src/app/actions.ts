6cef78fd569e1a1a9e4f753d61544a53e116d2a6
that-jamal <varg.jamil@gmail.com>
2024-04-09 09:24:27 +0200

    update

---
+++ src/app/actions.ts
@@ -0,0 +1,20 @@
+"use server"
+import { db } from "@/utils/db"
+
+export async function addTransaction(
+    count: number,
+    symbol: string,
+    price: string
+) {
+    console.log("buying", count, "of", symbol, "at", price)
+    // Add count, symbol and price to a database
+    await db(
+        "INSERT INTO transactions (units, symbol, purchasePrice) VALUES ($1, $2, $3)",
+        [count, symbol, Number.parseFloat(price)]
+    )
+}
+
+export async function getTransactions() {
+    const res = await db("SELECT * FROM transactions")
+    return res.rows
+}
---
+++ src/comonents/Transactions.tsx
@@ -0,0 +1,57 @@
+import { getTransactions } from "@/app/actions";
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/comonents/ui/table";
+
+function calcResult(oldPrice, newPrice) {
+    return ((newPrice - oldPrice) / oldPrice) * 100;
+}
+
+export default async function Transactions(props) {
+    const data = await getTransactions();
+
+    return (
+        <div className="w-full">
+            <h1 className="text-3xl font-semibold pb-2">Transactions</h1>
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="w-[100px]">Symbol</TableHead>
+                        <TableHead className="w-[100px] text-right">Count</TableHead>
+                        <TableHead className="text-right">Price</TableHead>
+                        <TableHead className="text-right">Change</TableHead>
+                    </TableRow>
+                </TableHeader>
+
+                <TableBody>
+                    {data.map((transaction) => (
+                        <TableRow key={transaction.id}>
+                            <TableCell>{transaction.symbol}</TableCell>
+                            <TableCell className="text-right">{transaction.units}</TableCell>
+                            <TableCell className="text-right">
+                                {Number(transaction.purchaseprice).toLocaleString("fi-FI", {
+                                    style: "currency",
+                                    currency: "USD",
+                                })}
+                            </TableCell>
+                            <TableCell className="text-right">
+                                {calcResult(
+                                    transaction.purchaseprice,
+                                    props.api.filter(
+                                        (crypto) => crypto.symbol === transaction.symbol,
+                                    )[0].priceUsd,
+                                ).toFixed(2)}
+                                %
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </div>
+    );
+}
